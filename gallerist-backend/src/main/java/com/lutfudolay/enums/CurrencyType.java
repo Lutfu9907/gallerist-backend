1d05ed6b6666b7e6eaac27aacf9f3de2cfabba02
LUTFU <dolaylutfu@gmail.com>
2025-04-30 22:03:27 +0300

    created database

---
+++ gallerist-backend/src/main/java/com/lutfudolay/enums/CurrencyType.java
@@ -0,0 +1,6 @@
+package com.lutfudolay.enums;
+
+public enum CurrencyType {
+
+	TL , USD
+}
---
+++ gallerist-backend/src/test/java/com/lutfudolay/gallerist_backend/GalleristBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutfudolay.gallerist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleristBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
