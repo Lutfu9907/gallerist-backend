a56c621f2a7beeee6d0e8f7379bba1aaa2a969a0
LUTFU <dolaylutfu@gmail.com>
2025-05-28 02:06:59 +0300

    Exception Handler class update

--- gallerist-backend/src/main/java/com/lutfudolay/handler/GlobalExceptionHandler.java
+++ gallerist-backend/src/main/java/com/lutfudolay/handler/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Flow.Subscriber;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
